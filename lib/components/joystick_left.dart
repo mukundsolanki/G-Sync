e8bd69550c71821dfead686394a7ed4d80923271
Mukund Solanki <mukundsolanki665@gmail.com>
2024-04-17 13:11:28 +0530

    Left and right joystick added

---
+++ lib/components/joystick_left.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_joystick/flutter_joystick.dart';
+
+const step = 10.0;
+
+class JoystickLeft extends StatefulWidget {
+  const JoystickLeft({Key? key}) : super(key: key);
+
+  @override
+  State<JoystickLeft> createState() => _JoystickLeftState();
+}
+
+class _JoystickLeftState extends State<JoystickLeft> {
+  double _x = 0;
+  double _y = 0;
+  JoystickMode _joystickMode = JoystickMode.all;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.green,
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Align(
+              alignment: const Alignment(-0.8, 0), // Left alignment
+              child: Joystick(
+                mode: _joystickMode,
+                listener: (details) {
+                  setState(() {
+                    _x += step * details.x;
+                    _y += step * details.y;
+                    print('X: $_x, Y: $_y');
+                  });
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
